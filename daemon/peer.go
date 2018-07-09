084004277006710e723f4ff803d84099496ce4ec
Roei Erez <roeierez@gmail.com>
2018-07-09 16:51:23 +0300

    fix log

--- daemon/peer.go
+++ daemon/peer.go
@@ -1239,7 +1239,7 @@ func (p *peer) logWireMessage(msg lnwire.Message, read bool) {
 
 // writeMessage writes the target lnwire.Message to the remote peer.
 func (p *peer) writeMessage(msg lnwire.Message) error {
-	peerLog.Infof("readNextMessage from peer (%x) started", p.PubKey())
+	peerLog.Infof("writeMessage (type = %v) to peer (%x) started", msg.MsgType(), p.PubKey())
 	// Simply exit if we're shutting down.
 	if atomic.LoadInt32(&p.disconnect) != 0 {
 		return ErrPeerExiting
