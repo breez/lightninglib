44cc52de5533b120088f5c7f3982e9a267b5fd8c
Yaacov Akiba Slama <ya@slamail.org>
2018-10-23 21:23:40 +0300

    Enable hintCache

--- daemon/chainregistry.go
+++ daemon/chainregistry.go
@@ -181,8 +181,8 @@ func newChainControlFromConfig(cfg *config, chanDB *channeldb.DB,
 		cleanUp func()
 	)
 
-	// Initialize disabled height hint cache within the chain directory.
-	hintCache, err := chainntnfs.NewHeightHintCache(chanDB, true)
+	// Initialize enabled height hint cache within the chain directory.
+	hintCache, err := chainntnfs.NewHeightHintCache(chanDB, false)
 	if err != nil {
 		return nil, nil, fmt.Errorf("unable to initialize height hint "+
 			"cache: %v", err)
