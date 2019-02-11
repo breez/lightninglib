5a2268024f2f5846e71acc816b86a5ae9fc9d2eb
Yaacov Akiba Slama <yaslama@gmail.com>
2019-02-11 17:26:19 +0200

    Fix typo

--- routing/conf_experimental.go
+++ routing/conf_experimental.go
@@ -5,7 +5,7 @@ package routing
 // Conf exposes the experimental command line routing configurations.
 type Conf struct {
 	AssumeChannelValid       bool `long:"assumechanvalid" description:"Skip checking channel spentness during graph validation. (default: false)"`
-	NoGraphUpdatingOnStartup bool `long:"nographupdatingonstartup" description "Skip updating the graph on startup. (default: false)"`
+	NoGraphUpdatingOnStartup bool `long:"nographupdatingonstartup" description:"Skip updating the graph on startup. (default: false)"`
 }
 
 // UseAssumeChannelValid returns true if the router should skip checking for
